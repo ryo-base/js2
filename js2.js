7b0dd2bd9d330924e99fc751a36c8962ad1fd0bf
ryo-base <ryo_base8@outlook.com>
2021-02-18 00:52:08 +0900

    return 動作しなかった

--- js2.js
+++ js2.js
@@ -4,7 +4,7 @@ const btn = document.getElementById('btn');
 
 //整数チェック
 const checkNum = function (x, y) {
-    if ((Number.isSafeInteger(x) === true) && (Number.isSafeInteger(x) === true)) {
+    if ((Number.isSafeInteger(x) === true) && (Number.isSafeInteger(y) === true)) {
         return true;
     } else {
         return false;
@@ -29,37 +29,41 @@ const outResult = function () {
         return;
     }
 
-    if ((fizz >= 100) && (buzz >= 100)){
+    if ((fizz >= 100) && (buzz >= 100)) {
         const li = document.createElement('li');
         li.textContent = '表示できるのは二桁の整数までです。';
         ul.appendChild(li);
         return;
     }
 
-    if (checkNum(fizz, buzz)) { //空欄以外なら
-        for (let i = 1; i <= 99; i++) {
-            const li = document.createElement('li');
-            if (i % fizz === 0 && i % buzz === 0) {
-                li.textContent = 'FizzBuzz' + i;
-                ul.appendChild(li);
-            } else if (i % fizz === 0) {
-                li.textContent = 'Fizz' + i;
-                ul.appendChild(li);
-            } else if (i % buzz === 0) {
-                li.textContent = 'Buzz' + i;
-                ul.appendChild(li);
-            }
-        };
-        return;
-    } 
-
     if (!checkNum(fizz, buzz)) {　//少数または文字なら
         const li = document.createElement('li');
         li.textContent = '整数値を入力してください';
         ul.appendChild(li);
+        return;
     }
+    
+    for (let i = 1; i <= 99; i++) {
+        const li = document.createElement('li');
+        if (i % fizz === 0 && i % buzz === 0) {
+            li.textContent = 'FizzBuzz' + i;
+            ul.appendChild(li);
+        } else if (i % fizz === 0) {
+            li.textContent = 'Fizz' + i;
+            ul.appendChild(li);
+        } else if (i % buzz === 0) {
+            li.textContent = 'Buzz' + i;
+            ul.appendChild(li);
+        }
+
+    };
+
     document.body.appendChild(ul);
-}
+};
+
+
+
+
 
 
 //実行
