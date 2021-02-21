0e071dc2c193f3c565f16f40b4f5a052b8a2e90a
ryo-base <ryo_base8@outlook.com>
2021-02-21 18:50:01 +0900

    修正分7

--- js2.js
+++ js2.js
@@ -1,16 +1,6 @@
 'use strict';
 
 const btn = document.getElementById('btn');
-
-//整数チェック
-const checkNum = function (x, y) {
-    if ((Number.isSafeInteger(x) === true) && (Number.isSafeInteger(y) === true)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 const ul = document.createElement('ul');
 
 //計算機能
@@ -26,48 +16,50 @@ const outResult = function () {
         const li = document.createElement('li');
         li.textContent = '整数値を入力してください';
         ul.appendChild(li);
+        document.body.appendChild(ul);
         return;
     }
+
 
     if ((fizz >= 100) && (buzz >= 100)) {
         const li = document.createElement('li');
         li.textContent = '表示できるのは二桁の整数までです。';
         ul.appendChild(li);
+        document.body.appendChild(ul);
         return;
     }
 
-    if (!checkNum(fizz, buzz)) {　//少数または文字なら
+    const pattern = /^[-]?([1-9]\d*|0)$/;;
+    if (!(pattern.test(fizz)) || !(pattern.test(buzz))) {
         const li = document.createElement('li');
-        li.textContent = '整数値を入力してください';
+        li.textContent = '整数を入力して下さい。';
         ul.appendChild(li);
+        document.body.appendChild(ul);
         return;
     }
-    
+
     for (let i = 1; i <= 99; i++) {
         const li = document.createElement('li');
         if (i % fizz === 0 && i % buzz === 0) {
             li.textContent = 'FizzBuzz' + i;
             ul.appendChild(li);
+            document.body.appendChild(ul);
         } else if (i % fizz === 0) {
             li.textContent = 'Fizz' + i;
             ul.appendChild(li);
+            document.body.appendChild(ul);
         } else if (i % buzz === 0) {
             li.textContent = 'Buzz' + i;
             ul.appendChild(li);
+            document.body.appendChild(ul);
         }
-
     };
 
-    document.body.appendChild(ul);
 };
-
-
-
-
-
 
 //実行
 btn.addEventListener('click', () => {
     ul.textContent = '';//前回分の結果を消去
     outResult();
 });
+
